fcd02b8c4e93d134e350482edc9e50399cc65d11
Hur Rizvi <92028524+jari-rizvi@users.noreply.github.com>
2022-08-17 06:46:46 -0700

    notificatios Fragment.

---
+++ app/src/main/java/com/teamx/zeus/ui/fragments/editProfile/EditProfileFragment.kt
@@ -0,0 +1,77 @@
+package com.teamx.zeus.ui.fragments.editProfile
+
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.navOptions
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.gson.JsonObject
+import com.teamx.zeus.BR
+import com.teamx.zeus.MainApplication
+import com.teamx.zeus.R
+import com.teamx.zeus.baseclasses.BaseFragment
+import com.teamx.zeus.data.remote.Resource
+import com.teamx.zeus.databinding.FragmentEditProfileBinding
+import com.teamx.zeus.databinding.FragmentForgotPassBinding
+import com.teamx.zeus.databinding.FragmentOTPBinding
+import com.teamx.zeus.databinding.FragmentSignInBinding
+import com.teamx.zeus.localization.LocaleManager
+import com.teamx.zeus.ui.fragments.SignInFragment.AuthViewModel
+import com.teamx.zeus.utils.DialogHelperClass
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONException
+
+
+@AndroidEntryPoint
+class EditProfileFragment() : BaseFragment<FragmentEditProfileBinding, AuthViewModel>() {
+
+    override val layoutId: Int
+        get() = R.layout.fragment_edit_profile
+    override val viewModel: Class<AuthViewModel>
+        get() = AuthViewModel::class.java
+    override val bindingVariable: Int
+        get() = BR.viewModel
+
+
+    private lateinit var options: NavOptions
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        options = navOptions {
+            anim {
+                enter = R.anim.enter_from_left
+                exit = R.anim.exit_to_left
+                popEnter = R.anim.nav_default_pop_enter_anim
+                popExit = R.anim.nav_default_pop_exit_anim
+            }
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/teamx/zeus/ui/fragments/notification/Notifications.kt
@@ -0,0 +1,9 @@
+package com.teamx.zeus.ui.fragments.notification
+
+class Notifications(
+    val notificationDescription: String,
+    val notificationTime: String,
+    val notificationImage: Int,
+    val boldWords: String
+)
+
