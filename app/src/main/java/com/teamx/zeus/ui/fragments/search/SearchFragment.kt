63331ccfc747c867ad8eaaeefe2880cbbefb73a9
Hur Rizvi <92028524+jari-rizvi@users.noreply.github.com>
2022-09-29 16:54:15 +0500

    serach fragment

--- app/src/main/java/com/teamx/zeus/ui/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/teamx/zeus/ui/fragments/search/SearchFragment.kt
@@ -1,4 +1,4 @@
-package com.teamx.zeus.ui.fragments.product
+package com.teamx.zeus.ui.fragments.search
 
 import android.os.Bundle
 import android.text.TextUtils
@@ -10,38 +10,38 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import androidx.navigation.navOptions
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.gson.JsonObject
-import com.squareup.picasso.Picasso
 import com.teamx.zeus.BR
 import com.teamx.zeus.MainApplication
 import com.teamx.zeus.R
 import com.teamx.zeus.baseclasses.BaseFragment
-import com.teamx.zeus.data.models.productBySlug.Category
 import com.teamx.zeus.data.remote.Resource
 import com.teamx.zeus.databinding.*
 import com.teamx.zeus.localization.LocaleManager
+import com.teamx.zeus.ui.fragments.notification.NotificationAdapter
+import com.teamx.zeus.ui.fragments.notification.Notifications
 import com.teamx.zeus.ui.fragments.otp.OtpViewModel
 import com.teamx.zeus.utils.DialogHelperClass
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.fragment_shop_home_page.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.json.JSONException
 
 
 @AndroidEntryPoint
-class ProductPreviewFragment() : BaseFragment<FragmentProductBinding, ProductPreviewViewModel>() {
+class SearchFragment() : BaseFragment<FragmentTrackBinding, OtpViewModel>() {
 
     override val layoutId: Int
-        get() = R.layout.fragment_product
-    override val viewModel: Class<ProductPreviewViewModel>
-        get() = ProductPreviewViewModel::class.java
+        get() = R.layout.fragment_track
+    override val viewModel: Class<OtpViewModel>
+        get() = OtpViewModel::class.java
     override val bindingVariable: Int
         get() = BR.viewModel
-
 
     private lateinit var options: NavOptions
 
@@ -57,34 +57,13 @@ class ProductPreviewFragment() : BaseFragment<FragmentProductBinding, ProductPre
                 popExit = R.anim.nav_default_pop_exit_anim
             }
 
+
         }
-
-        mViewModel.productPreview()
-
-        mViewModel.productPreviewResponse.observe(requireActivity(), Observer {
-            when (it.status) {
-                Resource.Status.LOADING -> {
-                    loadingDialog.show()
-                }
-                Resource.Status.SUCCESS -> {
-                    loadingDialog.dismiss()
-                    it.data?.let { data ->
-                        mViewDataBinding.ProductName.text = data.name
-                        mViewDataBinding.productDescriptio.text = data.description
-                        mViewDataBinding.productPrice.text = data.price.toString()+" AED"
-                        Picasso.get().load(data.image).into(mViewDataBinding.img)
-
-                    }
-                }
-                Resource.Status.ERROR -> {
-                    loadingDialog.dismiss()
-                    DialogHelperClass.errorDialog(requireContext(), it.message!!)
-                }
-            }
-        })
 
 
     }
+
+
 
 
 }
